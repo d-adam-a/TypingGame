eeff72f746ee0a63f7973b255c0107334cc2d1c8
Arie <Rozindzakwan@gmail.com>
2023-01-15 23:20:20 +0700

    Add project files.

---
+++ TypingGame/Models/Stats.cs
@@ -0,0 +1,37 @@
+﻿namespace TypingGame.Models
+{
+    public class Stats
+    {
+        public int Correct { get; set; }
+        public int Missed { get; set; }
+        public int Total { get; set; }
+        public int Accuracy { get; set; }
+
+        /*public int Total
+        {
+            get
+            {
+                return Correct + Missed;
+            }
+            set { }
+        }
+        public int Accuracy
+        {
+            get
+            {
+                return Correct * 100 / (Correct + Missed);
+            }
+            set { }
+        }*/
+
+        public void CountTotal()
+        {
+            Total = Correct + Missed;
+        }
+
+        public void CountAccuracy()
+        {
+            Accuracy = Correct * 100 / (Correct + Missed);
+        }
+    }
+}
